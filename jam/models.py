1b583faef4b7a39a5de4bc49d9164cec015a8571
David Allemang <allemang.d@gmail.com>
2019-03-06 21:11:53 -0500

    starting contest model

--- jam/models.py
+++ jam/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Problem(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    def get_absolute_url(self):
+        return reverse('jam:problem', kwargs=dict(slug=self.slug))
+
+
+class Contest(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    problem = models.ManyToManyField(Problem)
--- jam/urls.py
+++ jam/urls.py
@@ -5,5 +5,8 @@ from jam import views
 app_name = 'jam'
 
 urlpatterns = [
-    path('', views.Index.as_view()),
+    path('', views.Index.as_view(), name='index'),
+    path('p/list', views.ProblemList.as_view(), name='problem-list'),
+    path('p/new', views.ProblemCreate.as_view(), name='problem-create'),
+    path('p/<slug>', views.ProblemDetail.as_view(), name='problem'),
 ]
