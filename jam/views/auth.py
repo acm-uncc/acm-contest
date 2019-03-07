06d2aa6dbbf68f7d96142a93cfeeb002d7e16f0c
David Allemang <allemang.d@gmail.com>
2019-03-07 02:09:38 -0500

    remove extra auth views

--- jam/views/auth.py
+++ jam/views/auth.py
@@ -4,29 +4,6 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 
-class LoginView(generic.FormView):
-    form_class = authforms.AuthenticationForm
-    success_url = reverse_lazy('jam:index')
-
-    template_name = 'registration/login.html'
-
-    def form_valid(self, form):
-        user = auth.authenticate(self.request, username=form.cleaned_data['username'],
-                                 password=form.cleaned_data['password'])
-
-        auth.login(self.request, user)
-
-        return super(LoginView, self).form_valid(form)
-
-
-class LogoutView(generic.RedirectView):
-    url = reverse_lazy('jam:index')
-
-    def get(self, request, *args, **kwargs):
-        auth.logout(request)
-        return super(LogoutView, self).get(request, *args, **kwargs)
-
-
 class SignupView(generic.FormView):
     form_class = authforms.UserCreationForm
     success_url = reverse_lazy('jam:index')
