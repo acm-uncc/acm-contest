2359169de3c9502616f0281e8eb429a4e724ccec
David Allemang <allemang.d@gmail.com>
2019-03-15 11:49:37 -0400

    get out of debug

--- acmcodejam/settings.py
+++ acmcodejam/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '10&08-9&16*%a@(_=*)b209emqs0-$t94ezz=-=e5#c32543m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
