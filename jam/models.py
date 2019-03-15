286e2051bd5af15635899f98b66970c4c0e7d7ba
David Allemang <allemang.d@gmail.com>
2019-03-15 11:40:19 -0400

    add time to submissions. visible in admin page.

--- jam/models.py
+++ jam/models.py
@@ -1,3 +1,6 @@
+import datetime
+
+import pytz
 from django.conf.global_settings import AUTH_USER_MODEL
 from django.db import models
 from django.urls import reverse
@@ -55,6 +58,7 @@ class Submission(models.Model):
     part = models.ForeignKey(Part, on_delete=models.CASCADE)
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
 
+    time = models.DateTimeField(default=datetime.datetime(1970, 1, 1, tzinfo=pytz.utc))
     submission = models.TextField()
     correct = models.BooleanField(default=False)
 
@@ -63,10 +67,17 @@ class Submission(models.Model):
 
     def save(self, *a, **kw):
         self.correct = normalize(self.submission) == normalize(self.part.solution)
+        self.time = datetime.datetime.now()
         super(Submission, self).save(*a, **kw)
 
         score, created = Score.objects.get_or_create(user=self.user)
         score.recompute()
 
     def __str__(self):
-        return f'{self.user.username} - {self.part} ({"correct" if self.correct else "incorrect"})'
+        return (
+            f'{self.user.username} - '
+            f'{self.part.problem.title} : {self.part.title} '
+            f'({self.part.problem.slug}-{self.part.slug}) - '
+            f'{"correct" if self.correct else "incorrect"} - '
+            f'{self.time:%m/%d/%y %I:%M %p}'
+        )
