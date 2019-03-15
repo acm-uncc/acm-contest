dbc659cf87a261cb1a4c080c65b3359325bc6d3c
David Allemang <allemang.d@gmail.com>
2019-03-15 00:42:51 -0400

    add css

--- acmcodejam/settings.py
+++ acmcodejam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,5 @@ MARKDOWN_FILTER_WHITELIST_TAGS = [
     'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
     'ul', 'ol', 'li'
 ]
+
+django_heroku.settings(locals())
